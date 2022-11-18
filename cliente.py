308bc21703ef325db9312370513fa20672906bfd
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-18 11:02:07 -0300

    [add]: Try Except when connecting to server

--- cliente.py
+++ cliente.py
@@ -7,34 +7,43 @@ PORT = 55551
 BUFFER_SIZE = 1024
 
 
-cmd.clear_screen()
-print('Tentando se conectar ao servidor...')
-print(f'HOST: {HOST},PORT:{PORT}')
-cmd.delay_loading(10)
-cmd.clear_screen()
+def main():
+    cmd.clear_screen()
+    print('Tentando se conectar ao servidor...')
+    print(f'HOST: {HOST},PORT:{PORT}')
+    cmd.delay_loading(10)
+    cmd.clear_screen()
 
-server = socket(AF_INET, SOCK_STREAM)
-server.connect((HOST, PORT))
+    try:
+        server = socket(AF_INET, SOCK_STREAM)
+        server.connect((HOST, PORT))
+    except:
+        print(colorama.Fore.LIGHTRED_EX +
+              '\n- Não foi possível se conectar ao servidor. Ele está ativo?')
+        return cmd.clear_terminal_color()
 
-# Instruções para o cliente
-msg = server.recv(BUFFER_SIZE)
-while msg.decode() != 'start':
-    print(msg.decode())
+    # Instruções para o cliente
     msg = server.recv(BUFFER_SIZE)
-
-print('\n')
-print("No que você está pensando?")
-# Loop de interação com o servidor
-while msg.decode() != 'close':
-    frase = input("> ")
-    server.send(frase.encode())
-
-    msg = server.recv(BUFFER_SIZE)
-    if msg.decode() != 'close':
+    while msg.decode() != 'start':
         print(msg.decode())
-        print('\n')
+        msg = server.recv(BUFFER_SIZE)
 
-print('\n')
-print(colorama.Fore.LIGHTRED_EX + f'- Conexão encerrada com o servidor')
-server.close()
-cmd.clear_terminal_color()
+    print('\n')
+    print("No que você está pensando?")
+    # Loop de interação com o servidor
+    while msg.decode() != 'close':
+        frase = input("> ")
+        server.send(frase.encode())
+
+        msg = server.recv(BUFFER_SIZE)
+        if msg.decode() != 'close':
+            print(msg.decode())
+            print('\n')
+
+    print('\n')
+    print(colorama.Fore.LIGHTRED_EX + f'- Conexão encerrada com o servidor')
+    server.close()
+    cmd.clear_terminal_color()
+
+
+main()
