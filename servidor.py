a9a32f18e927f6a8d799b20ca406cdabe55a925e
Igor Rocha <igor_roc@hotmail.com.br>
2022-11-18 09:22:32 -0300

    [add]: Analise sentence function

--- servidor.py
+++ servidor.py
@@ -25,12 +25,12 @@ while True:
     while True:
         msg = clientSocket.recv(1024)
         msg = msg.decode()
-        print(msg)
+
         if msg == 'sair':
             print(colorama.Fore.LIGHTRED_EX + f'- Conexão encerrada com o cliente: {address}')
             clientSocket.send('close'.encode())
             # clientSocket.close()
             break
-        saidaStem
-        extraido = ia.extratorPalavras(saidaStem)
-        print(ia.classificador.classify(extraido))
+
+        fraseInterpretada = ia.interpreta_frase(msg)
+        clientSocket.send(fraseInterpretada.encode())
--- utils/inteligencia.py
+++ utils/inteligencia.py
@@ -5,46 +5,64 @@ nltk.download('stopwords')
 nltk.download('rslp')
 stopWords = nltk.corpus.stopwords.words('portuguese')
 
+
 def aplicaStemmer(texto):
-  stemmer=nltk.stem.RSLPStemmer()
-  frasesStemming=[]
-  for (palavras,emocao) in texto:
-    comStemming=[stemmer.stem(p) for p in palavras.split() if p not in stopWords]
-    frasesStemming.append((comStemming,emocao))
-  return frasesStemming
+    stemmer = nltk.stem.RSLPStemmer()
+    frasesStemming = []
+    for (palavras, emocao) in texto:
+        comStemming = [stemmer.stem(p)
+                       for p in palavras.split() if p not in stopWords]
+        frasesStemming.append((comStemming, emocao))
+    return frasesStemming
+
 
 def buscaPalavras(frases):
-  todasPalavras=[]
-  for palavras,emocao in frases:
-    todasPalavras.extend(palavras)
-  return todasPalavras
+    todasPalavras = []
+    for palavras, emocao in frases:
+        todasPalavras.extend(palavras)
+    return todasPalavras
+
 
 def buscaFrequencia(palavras):
-  frequencias=nltk.FreqDist(palavras)
-  return frequencias
+    frequencias = nltk.FreqDist(palavras)
+    return frequencias
+
 
 def palavrasUnicas(frequencias):
-  unicas = frequencias.keys()
-  return unicas
+    unicas = frequencias.keys()
+    return unicas
+
 
 def extratorPalavras(documento):
-  saida={}
-  doc=set(documento)
-  for pu in PU:
-    if pu in doc:
-      saida['%s' % pu]=True
-    else:
-      saida['%s' % pu]=False
-  return saida
+    saida = {}
+    doc = set(documento)
+    for pu in PU:
+        if pu in doc:
+            saida['%s' % pu] = True
+        else:
+            saida['%s' % pu] = False
+    return saida
 
-listaTotalPalavras=buscaPalavras(aplicaStemmer(db.BASE))
+
+listaTotalPalavras = buscaPalavras(aplicaStemmer(db.BASE))
 len(listaTotalPalavras)
 
-frequencia=buscaFrequencia(listaTotalPalavras)
+frequencia = buscaFrequencia(listaTotalPalavras)
 frequencia.most_common(8)
 
 PU = palavrasUnicas(frequencia)
 
-frasesComStem=aplicaStemmer(db.BASE)
-baseCompleta=nltk.classify.apply_features(extratorPalavras,frasesComStem)
+frasesComStem = aplicaStemmer(db.BASE)
+baseCompleta = nltk.classify.apply_features(extratorPalavras, frasesComStem)
 classificador = nltk.NaiveBayesClassifier.train(baseCompleta)
+
+
+def interpreta_frase(msg):
+    saidaStem = []
+    stem = nltk.RSLPStemmer()
+    for p in msg.split():
+        if p not in stopWords:
+            saidaStem.append(stem.stem(p))
+    saidaStem
+    extraido = extratorPalavras(saidaStem)
+    return (classificador.classify(extraido))
